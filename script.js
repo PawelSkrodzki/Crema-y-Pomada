30f33f36207b46dd6e92675472df902a0ef656d2
Pawel Skrodzki <pawel.skrodzki14@gmail.com>
2020-12-07 20:43:54 +0100

    style and script fixes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const clockSpan = document.getElementById("time")
 const dateSpan = document.getElementById("date")
 
@@ -10,10 +12,17 @@ showTime = () =>{
     const day = today.getDate()
     const month = today.getMonth() + 1
 
-    clockSpan.innerHTML = `${hours}/${minutes}    `
+    clockSpan.innerHTML = `${hours}:${minutes}    `
     dateSpan.innerHTML = `${day}.${month}`
 }
 setInterval(showTime, 1000)
 
+
+const scroll = new Scrooth({
+  element: window,
+  strength: 11,
+  acceleration: 1.5,
+  deceleration: 0.975,
+});
 
 
