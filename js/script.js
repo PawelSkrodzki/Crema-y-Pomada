e64822f1ac0e51d7d6e5f60500e6f2c77d9d4468
Pawel Skrodzki <pawel.skrodzki14@gmail.com>
2020-12-10 13:52:00 +0100

    random photo script added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 
-
 const clockSpan = document.getElementById("time")
 const dateSpan = document.getElementById("date")
 
@@ -7,7 +6,6 @@ showTime = () =>{
     const today = new Date()
     const hours = today.getHours()
     const minutes = today.getMinutes()
-    const secounds = today.getSeconds()
 
     const day = today.getDate()
     const month = today.getMonth() + 1
@@ -16,6 +14,17 @@ showTime = () =>{
     dateSpan.innerHTML = `${day}.${month}`
 }
 setInterval(showTime, 1000)
+
+const photo1 = document.querySelector('.central-photo1')
+const photo2 = document.querySelector('.central-photo2')
+const photo3 = document.querySelector('.central-photo3')
+
+const photoArray = [photo1, photo2, photo3]
+
+const selectedPhoto = photoArray[Math.floor(Math.random() * photoArray.length)];
+
+selectedPhoto.style.visibility = "visible";
+
 
 
 
